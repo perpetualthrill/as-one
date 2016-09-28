324a077ae61145acb7a318347c7973493163ec82
Jack Chatterton <chattj@gmail.com>
2016-09-28 03:06:16 -0700

    attempt to read digital pins

--- arduino_olimex_1/arduino_olimex_1.ino
+++ arduino_olimex_1/arduino_olimex_1.ino
@@ -1,6 +1,11 @@
 
-const int p1input = A0;
-const int p3input = A1;
+//const int p1input = A0;
+//const int p3input = A1;
+const int p1input = 6;
+const int p3input = 7;
+
+int p1value = -999;
+int p3value = -999;
 
 void setup() {
   pinMode(p1input, INPUT);
@@ -10,12 +15,16 @@ void setup() {
 }
 
 void loop() {
-  int p1value = analogRead(p1input);
-  int p3value = analogRead(p3input);
-  Serial.print("p1: ");
-  Serial.print(p1value);
-  Serial.print(" p3: ");
-  Serial.println(p3value);
-  delay(1);
+  int new_p1value = digitalRead(p1input);
+  int new_p3value = digitalRead(p3input);
+  if (new_p1value != p1value || new_p3value != p3value) {
+    p1value = new_p1value;
+    p3value = new_p3value;
+    Serial.print("p1: ");
+    Serial.print(p1value);
+    Serial.print(" p3: ");
+    Serial.println(p3value);
+  //  delay(1);
+  }
 }
 
