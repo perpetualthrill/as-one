ea232dac711130ef36939bbe76c591a1bb880695
Jack Chatterton <chattj@gmail.com>
2016-09-28 23:43:26 -0700

    back to analog w/ abs(), no love

--- arduino_olimex_1/arduino_olimex_1.ino
+++ arduino_olimex_1/arduino_olimex_1.ino
@@ -1,8 +1,8 @@
 
-//const int p1input = A0;
-//const int p3input = A1;
-const int p1input = 6;
-const int p3input = 7;
+const int p1input = A0;
+const int p3input = A1;
+//const int p1input = 6;
+//const int p3input = 7;
 
 int p1value = -999;
 int p3value = -999;
@@ -15,9 +15,10 @@ void setup() {
 }
 
 void loop() {
-  int new_p1value = digitalRead(p1input);
-  int new_p3value = digitalRead(p3input);
-  if (new_p1value != p1value || new_p3value != p3value) {
+  int new_p1value = analogRead(p1input);
+  int new_p3value = analogRead(p3input);
+  
+  if (abs(new_p1value - p1value) > 2 || abs(new_p3value - p3value) > 2) {
     p1value = new_p1value;
     p3value = new_p3value;
     Serial.print("p1: ");
