091726d4a87ad286e33a23563e4bdd7c5ef3b9d3
Jack Chatterton <chattj@gmail.com>
2019-04-13 21:06:38 -0700

    fill in setup+test code for handheld-2

--- sketchbook-arduino/handheld-2/handheld-2.ino
+++ sketchbook-arduino/handheld-2/handheld-2.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>
 
+const int ANALOG_INPUT_3 = A3;
+
+// Adjust this number to avoid noise when idle
+const int THRESHOLD = 550;
+
+PulseSensorPlayground pulseSensor(1);
+
+void setup() {
+  Serial.begin(115200);
+
+  pulseSensor.analogInput(ANALOG_INPUT_3, 0);
+  pulseSensor.setThreshold(THRESHOLD);
+  pulseSensor.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int sample = pulseSensor.getLatestSample(0);
+  int bpm = pulseSensor.getBeatsPerMinute(0);
+  //int fixedSample = sample / 4;
+  int fixedSample = sample;
+  Serial.print(fixedSample);
+  Serial.print(",");
+  Serial.println(bpm);
+  delay(250);
 }
