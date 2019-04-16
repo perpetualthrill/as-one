f59b93e0ed38c85692bb040ae2086cd41f90f272
Jack Chatterton <chattj@gmail.com>
2019-04-16 09:23:14 -0700

    wire up second sensor

--- sketchbook-arduino/handheld-2/handheld-2.ino
+++ sketchbook-arduino/handheld-2/handheld-2.ino
@@ -1,28 +1,59 @@
-#define USE_ARDUINO_INTERRUPTS true
+//#define USE_ARDUINO_INTERRUPTS true
+#define USE_ARDUINO_INTERRUPTS false
 #include <PulseSensorPlayground.h>
 
-const int ANALOG_INPUT_3 = A3;
 
 // Adjust this number to avoid noise when idle
-const int THRESHOLD = 550;
+const int THRESHOLD = 500;
 
-PulseSensorPlayground pulseSensor(1);
+const int PEAK_TROUGH = 30;
+const int MAX_SIGNAL = 1024;
+
+const int SENSOR_COUNT = 2;
+PulseSensorPlayground pulseSensor(SENSOR_COUNT);
+
+const int REPORT_PERIOD_MS = 25;
+long lastReport;
 
 void setup() {
   Serial.begin(115200);
 
-  pulseSensor.analogInput(ANALOG_INPUT_3, 0);
+  pulseSensor.analogInput(A3, 0);
+  pulseSensor.analogInput(A2, 1);
   pulseSensor.setThreshold(THRESHOLD);
   pulseSensor.begin();
+
+  lastReport = millis();
 }
 
 void loop() {
-  int sample = pulseSensor.getLatestSample(0);
-  int bpm = pulseSensor.getBeatsPerMinute(0);
-  //int fixedSample = sample / 4;
-  int fixedSample = sample;
-  Serial.print(fixedSample);
-  Serial.print(",");
-  Serial.println(bpm);
-  delay(250);
+
+  if ((millis() - lastReport) > REPORT_PERIOD_MS) {
+
+    for (int i = 0; i < SENSOR_COUNT; i++) {
+      int sample = pulseSensor.getLatestSample(i);
+      int bpm = pulseSensor.getBeatsPerMinute(i);
+
+      // Remove obnoxious connection noise
+      int fixedSample = sample;
+      if (fixedSample > (MAX_SIGNAL - PEAK_TROUGH)) {
+        fixedSample = PEAK_TROUGH;
+      } else if (fixedSample < PEAK_TROUGH) {
+        fixedSample = 0;
+      }
+      fixedSample = fixedSample / 4;
+
+      Serial.print(fixedSample);
+      Serial.print(",");
+      Serial.print(bpm);
+      Serial.print(",");
+    }
+
+    Serial.println();
+    lastReport = millis();
+  }
+
+  // Need to do this constantly because we're not using the timer
+  pulseSensor.sawNewSample();
+
 }
