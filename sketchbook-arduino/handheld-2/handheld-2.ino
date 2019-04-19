fce0720fa098f31b246b318895d98a8893745f4c
Jack Chatterton <chattj@gmail.com>
2019-04-18 23:13:01 -0700

    four sensors plus 'in beat' int

--- sketchbook-arduino/handheld-2/handheld-2.ino
+++ sketchbook-arduino/handheld-2/handheld-2.ino
@@ -9,7 +9,7 @@ const int THRESHOLD = 500;
 const int PEAK_TROUGH = 30;
 const int MAX_SIGNAL = 1024;
 
-const int SENSOR_COUNT = 2;
+const int SENSOR_COUNT = 4;
 PulseSensorPlayground pulseSensor(SENSOR_COUNT);
 
 const int REPORT_PERIOD_MS = 25;
@@ -24,8 +24,10 @@ void setup() {
   analogSetAttenuation(ADC_11db); // highest attenuation, used for 3.3v input
 
 
-  pulseSensor.analogInput(A3, 0);
-  pulseSensor.analogInput(A6, 1);
+  pulseSensor.analogInput(A6, 0); // board D34
+  pulseSensor.analogInput(A7, 1); // board D35
+  pulseSensor.analogInput(A4, 2); // board D32
+  pulseSensor.analogInput(A5, 3); // board D33
   pulseSensor.setThreshold(THRESHOLD);
   pulseSensor.begin();
 
@@ -36,20 +38,22 @@ void loop() {
 
   if ((millis() - lastReport) > REPORT_PERIOD_MS) {
 
+    int inBeatCount = 0;
     for (int i = 0; i < SENSOR_COUNT; i++) {
       int sample = pulseSensor.getLatestSample(i);
       int bpm = pulseSensor.getBeatsPerMinute(i);
+      if (pulseSensor.isInsideBeat(i)) inBeatCount++;
 
       int fixedSample = sample;
       fixedSample = fixedSample / 4;
 
       Serial.print(fixedSample);
       Serial.print(",");
-      Serial.print(bpm);
-      Serial.print(",");
+//      Serial.print(bpm);
+//      Serial.print(",");
     }
 
-    Serial.println();
+    Serial.println(inBeatCount * 40);
     lastReport = millis();
   }
 
