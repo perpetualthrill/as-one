d20184fb7621dc6d0220dbc7345ab65d5e74fa1e
Jack Chatterton <chattj@gmail.com>
2019-04-21 17:39:54 -0700

    add motor feedback!

--- sketchbook-arduino/handheld-2/handheld-2.ino
+++ sketchbook-arduino/handheld-2/handheld-2.ino
@@ -6,6 +6,7 @@ const int THRESHOLD = 500;
 
 // Reasonable (?) thresholds for resting heart rate
 const int MAX_BPM = 135;
+const int MIN_INTERVAL_MS = (int)(((float)60 / MAX_BPM) * 1000);
 const int MIN_BPM = 50;
 
 const int SENSOR_COUNT = 4;
@@ -16,30 +17,38 @@ int bpmBuffer[SENSOR_COUNT];
 
 // 25 ms => 40 hz
 const int REPORT_PERIOD_MS = 25;
-long lastReport;
+long lastReport = millis();
+
+long nextMotor = 0;
+long lastMotor = millis();
+const int MOTOR_PERIOD_MS = 180;
 
 void setup() {
   Serial.begin(115200);
-
 
   // Configure the ESP32 ADC. This is from esp32-hal-adc.h
   analogSetWidth(10); // range 0 - 1023, as expected by pulsesensor
   analogSetAttenuation(ADC_11db); // highest attenuation, used for 3.3v input
 
-
+  // Configure pulseSensor library and gather an initial sample
   pulseSensor.analogInput(A6, 0); // board D34
   pulseSensor.analogInput(A7, 1); // board D35
   pulseSensor.analogInput(A4, 2); // board D32
   pulseSensor.analogInput(A5, 3); // board D33
   pulseSensor.setThreshold(THRESHOLD);
   pulseSensor.begin();
+  pulseSensor.sawNewSample();
 
-  lastReport = millis();
+  // Configure outputs
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
 }
 
 void loop() {
+  long now = millis();
 
-  if ((millis() - lastReport) > REPORT_PERIOD_MS) {
+  if ((now - lastReport) > REPORT_PERIOD_MS) {
 
     int inBeatCount = 0;
     for (int i = 0; i < SENSOR_COUNT; i++) {
@@ -67,23 +76,41 @@ void loop() {
     for (int i = 0; i < (SENSOR_COUNT - 1); i++) {
       for (int j = i + 1; j < SENSOR_COUNT; j++) {
         // close enough!
-        if (bpmBuffer[j] - 1 <= bpmBuffer[i] <= bpmBuffer[j] + 1) hasConsensus = true;
+        if ((bpmBuffer[i] >= MIN_BPM) &&
+            ((bpmBuffer[j] - 1 == bpmBuffer[i]) ||
+             (bpmBuffer[j] == bpmBuffer[i]) ||
+             (bpmBuffer[j] + 1 == bpmBuffer[i]))) {
+          hasConsensus = true;
+        }
       }
     }
 
     if (hasConsensus) {
       Serial.println(inBeatCount * 40);
+      // Trigger motor feedback if enough time has passed
+      if ((inBeatCount > 0) &&
+          (now > (nextMotor + MIN_INTERVAL_MS))) {
+        lastMotor = nextMotor;
+        nextMotor = now;
+      }
     } else {
       Serial.println(0);
     }
 
-    lastReport = millis();
+    // Turn motor on or off as necessary
+    if ((now >= nextMotor) && (now <= (nextMotor + MIN_INTERVAL_MS))) {
+      digitalWrite(13, HIGH);
+    } else {
+      digitalWrite(13, LOW);
+    }
+
+    lastReport = now;
   }
 
-  // Need to do this constantly because we're not using the timer
+  // Need to call this constantly because we're not using the timer
   pulseSensor.sawNewSample();
 
-  // But not too constantly
+  // ... but not too constantly
   delay(1);
 
 }
