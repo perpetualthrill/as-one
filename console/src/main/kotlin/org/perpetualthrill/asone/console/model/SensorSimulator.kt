cc4ac9d9bf2b38b561bd433b5e8860bdc7008317
Jack Chatterton <jack.chatterton@twistle.com>
2019-05-19 16:21:45 -0700

    move csv data source into simulator

--- console/src/main/kotlin/org/perpetualthrill/asone/console/model/SensorSimulator.kt
+++ console/src/main/kotlin/org/perpetualthrill/asone/console/model/SensorSimulator.kt
@@ -1,7 +1,14 @@
 package org.perpetualthrill.asone.console.model
 
 import io.reactivex.Observable
-import org.perpetualthrill.asone.console.io.SensorDataSource
+import org.xbib.jdbc.io.TableReader
+import java.io.InputStreamReader
+import java.io.Reader
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
+import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -9,61 +16,109 @@ class SensorSimulator
 @Inject
 constructor() {
 
-    @Inject lateinit var sensorDataSource: SensorDataSource
+    private val connection: Connection
 
-    private val quiescentResults: SensorDataSource.ResultIterator by lazy {
-        sensorDataSource.getQuiescentResults()
+    init {
+        connection = DriverManager.getConnection("jdbc:xbib:csv:class:"+ResourceFileReader::class.java.name)
     }
 
     private var name = "unstarted!"
     private var state = SimulatorState.QUIESCENT
     private var finished = false
 
-    // must lazy initialize so that the lateinit vars are filled
-    // in by the time this is used. i.e. not at simulator init time
-    private val resultIteratorForState by lazy { mapOf(
-        SimulatorState.QUIESCENT to quiescentResults
-    )}
-
     val readingStream: Observable<Sensor.Reading> = Observable
         .interval(20, TimeUnit.MILLISECONDS)
         .takeWhile { !finished }
         .map { checkStateAndReturnReading() }
 
-
     fun start(sensorName: String) {
         name = sensorName
     }
 
-    private fun checkedGetIteratorForState(state: SimulatorState): SensorDataSource.ResultIterator {
-        return resultIteratorForState[state] ?: throw RuntimeException("Oh dear, the SensorSimulator state machine broke!")
-    }
-
-    fun checkStateAndReturnReading(): Sensor.Reading {
-        val currentStateIterator = checkedGetIteratorForState(state)
+    private fun checkStateAndReturnReading(): Sensor.Reading {
+        val currentStateIterator = resultIteratorOrThrow(state)
         val next = if (currentStateIterator.hasNext) {
             currentStateIterator.getNext()
         } else {
             state = state.nextState
-            val newStateIterator = checkedGetIteratorForState(state)
-            newStateIterator.reset()
-            newStateIterator.getNext()
+            val nextStateIterator = resultIteratorOrThrow(state)
+            nextStateIterator.reset()
+            nextStateIterator.getNext()
         }
         return Sensor.Reading(name, next.s1, next.s2, next.s3, next.s4)
     }
 
     fun finish() {
         finished = true
-        sensorDataSource.finish()
+        connection.close() // also closes all resultsets
+    }
+
+    private fun resultIteratorOrThrow(state: SimulatorState): ResultIterator {
+        return allResults[state] ?: throw RuntimeException("Oh dear! The sensor simulator state machine broke")
+    }
+
+    private val quiescentResults: ResultIterator by lazy {
+        // val countStmt = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+        val statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
+        val resultSet = statement.executeQuery("select s1, s2, s3, s4, state from log-data-20190512 where state = 'quiescent'")
+        ResultIterator(resultSet, false)
+    }
+
+    private val allResults: Map<SimulatorState, ResultIterator> by lazy {
+        mapOf(SimulatorState.QUIESCENT to quiescentResults)
     }
 
     private enum class SimulatorState {
 
         QUIESCENT {
             override val nextState = QUIESCENT
+            override val stateColumnValue = "quiescent"
         };
 
         abstract val nextState: SimulatorState
+        abstract val stateColumnValue: String
     }
 
+    // Note! This class does a lot of real borderline shit. Check hasNext before calling getNext
+    // on a one shot result set or it will throw
+    inner class ResultIterator(private val resultSet: ResultSet, private val oneShot: Boolean) {
+
+        val hasNext = if (oneShot) !resultSet.isAfterLast else true
+
+        fun getNext(): Sensor.Reading {
+            val gotNext = resultSet.next()
+            if (!gotNext && !oneShot) {
+                reset()
+            }
+            return sensorReadingFromCurrentRow()
+        }
+
+        fun reset() {
+            resultSet.first()
+        }
+
+        private fun sensorReadingFromCurrentRow(): Sensor.Reading {
+            return Sensor.Reading(
+                sensorName = name,
+                s1 = resultSet.getInt(1),
+                s2 = resultSet.getInt(2),
+                s3 = resultSet.getInt(3),
+                s4 = resultSet.getInt(4)
+            )
+        }
+
+    }
+
+    class ResourceFileReader : TableReader {
+        override fun getReader(statement: Statement, tableName: String): Reader {
+            val inputStream = javaClass.getResourceAsStream("/$tableName.csv")
+            return InputStreamReader(inputStream)
+        }
+
+        override fun getTableNames(connection: Connection): List<String> {
+            val v = Vector<String>()
+            v.add("log-data-20190512")
+            return v
+        }
+    }
 }
