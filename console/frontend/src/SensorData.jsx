0055149dc077551e2e857320715f0599d2659cff
Jack Chatterton <jack.chatterton@twistle.com>
2019-05-27 01:47:49 -0700

    poll sensor data at interval

--- console/frontend/src/SensorData.jsx
+++ console/frontend/src/SensorData.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-import { useFetch } from './hooks'
+import React, { useState } from 'react'
+import { useInterval } from './hooks'
+import axios from 'axios'
+import logger from './logger'
 
 function SensorData () {
-  const [data, loading] = useFetch('/sensors/latest')
+  const URL_LATEST = '/sensors/latest'
+
+  let [data, setData] = useState([])
+  let [loading, setLoading] = useState(true)
+
+  async function pollServerAndUpdate () {
+    try {
+      const response = await axios.get(URL_LATEST)
+      setData(response.data)
+      setLoading(false)
+    } catch (error) {
+      logger.error(error)
+    }
+  }
+
+  useInterval(() => {
+    pollServerAndUpdate()
+  }, 1000)
 
   return (
     <>
---
+++ console/frontend/src/logger.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+// TODO: Replace this with a real logger, I suppose
+
+const logger = {
+
+  error (value) {
+    console.error(value)
+  },
+
+  log (value) {
+    console.log(value)
+  }
+
+}
+
+export default logger
