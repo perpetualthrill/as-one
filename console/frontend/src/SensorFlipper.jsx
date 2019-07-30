e7901e9000b18fe89d761330744e41cc4a4ce535
Jack Chatterton <jack.chatterton@twistle.com>
2019-07-30 10:46:36 -0700

    add sensor flipper button

---
+++ console/frontend/src/SensorFlipper.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+import logger from './logger'
+
+const SENSOR_FLIP_PATH = '/game/flipSensors'
+
+function SensorFlipper () {
+
+  async function postToFlip() {
+    try {
+      const response = await axios.post(SENSOR_FLIP_PATH)
+      logger.log("flipped sensor: "+response)
+    } catch (error) {
+      logger.error(error)
+    }
+  }
+
+  /* eslint-disable jsx-a11y/accessible-emoji */
+  return (
+    <Row>
+        <Col md={5} sm={0} />
+        <Col sm={2}>
+          <Button
+            className="btn-block"
+            onPointerUp={() => {
+              postToFlip()
+            }}>↖️ FLIP ↗️</Button>
+        </Col>
+        <Col md={5} sm={0} />
+    </Row>
+  )
+  /* eslint-enable jsx-a11y/accessible-emoji */
+
+}
+
+export { SensorFlipper }
